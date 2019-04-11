dc2c40048837093677fd969e1f1aba1e410bd543
Ismaïl <nguyen.ismail@gmail.com>
2019-04-12 00:04:41 +0200

    Add speakers.json

--- src/app.speakers.js
+++ src/app.speakers.js
@@ -1,58 +1,70 @@
 $(document).ready(() => {
-            $.getJSON('//raw.githubusercontent.com/ncraftsconf/newcrafts19/master/schedule.json')
+            $.getJSON('./speakers.json')
                 .then(resp => {
-                    let speakers = [];
-
-                    for (var i = 0; i < resp.days.length; i++)
-                    {
-                        for (var j = 0; j < resp.days[i].rooms.length; j++)
-                        {
-                            for (var k = 0; k < resp.days[i].rooms[j].events.length; k++)
-                            {
-                                let speaker = {
-                                    speakerPhoto: resp.days[i].rooms[j].events[k].speakerPhoto,
-                                    speakerName: resp.days[i].rooms[j].events[k].speakerName,
-                                };
-
-                                speakers.push(speaker);
-                            }
-                        }
-                    }
-
-                    // Remove duplicates
-                    speakers = speakers.filter((speaker, index, self) =>
-                        index === self.findIndex((s) => (
-                            s.speakerName === speaker.speakerName
-                        ))
-                    );
+                    let speakers = resp.speakers;
 
                     for (var i = 0; i < speakers.length; i++) {
+                        
                         let speaker = speakers[i];
 
                         let speakerHtml = `
                             <div class="col-sm-4">
                                 <div class="speaker">
                                     <div class="photo-wrapper rounded">
-                                        <img data-src="${ speaker.speakerPhoto }" alt="${ speaker.speakerName }" class="img-responsive lazyload" data-sizes="auto" />
+                                        <img data-src="${ speaker.image }" alt="${ speaker.speakerName }" class="img-responsive lazyload" data-sizes="auto" />
                                     </div>
                                     <h3 class="name">${ speaker.speakerName }</h3>
                                     <p class="text-alt">
-                                        <small>${ speaker.shortDescription }</small>
+                                        <small>${ speaker.bioxs }</small>
                                     </p>
                                     <p class="about">
-                                        ${ speaker.fullDescription }
+                                        ${ speaker.bio }
                                     </p>
                                     <ul class="speaker-socials">
-                                        <li>
-                                            <a target="_blank" href="${ speaker.twitter }">
-                                                <span class="fa fa-twitter"></span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a target="_blank" href="${ speaker.github }">
-                                                <span class="fa fa-github"></span>
-                                            </a>
-                                        </li>
+                        `;
+
+
+                        if (speaker.twitter !== '') {
+                            speakerHtml += `
+                                <li>
+                                    <a target="_blank" href="${ speaker.twitter }">
+                                        <span class="fa fa-twitter"></span>
+                                    </a>
+                                </li>
+                            `;
+                        }
+
+                        if (speaker.git !== '') {
+                            speakerHtml += `
+                                <li>
+                                    <a target="_blank" href="${ speaker.git }">
+                                        <span class="fa fa-github"></span>
+                                    </a>
+                                </li>
+                            `;
+                        }
+
+                        if (speaker.website !== '') {
+                            speakerHtml += `
+                                <li>
+                                    <a target="_blank" href="${ speaker.website }">
+                                        <span class="fa fa-link"></span>
+                                    </a>
+                                </li>
+                            `;
+                        }
+
+                        if (speaker.book !== '') {
+                            speakerHtml += `
+                                <li>
+                                    <a target="_blank" href="${ speaker.book }">
+                                        <span class="fa fa-book"></span>
+                                    </a>
+                                </li>
+                            `;
+                        }
+
+                        speakerHtml += `
                                     </ul>
                                 </div>
                             </div>

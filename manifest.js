972185af2b56da890af9c27d880db6d5600028b3
Ismaïl <nguyen.ismail@gmail.com>
2019-05-11 10:00:08 +0200

    Move mobile app to root folder

--- manifest.js
+++ manifest.js
@@ -6,46 +6,42 @@
     "display": "standalone",
     "start_url": "/",
     "short_name": "NewCrafts Paris",
-    "theme_color": "#262627",
+    "theme_color": "transparent",
     "description": "NewCrafts Paris the 16 & 17 May, an ephemeral learning ground for professional developers who care for quality code and bettering their practices.",
     "orientation": "any",
     "background_color": "#262627",
     "related_applications": [],
     "prefer_related_applications": false,
-    "icons": [
-        {
-            "src": "/assets/img/favicon.ico",
-            "sizes": "56x64"
-        },
-        {
-            "src": "/assets/img/logo.png",
-            "type": "image/png",
-            "sizes": "287x334"
-        },
-        {
-            "src": "/assets/img/logo.png",
-            "sizes": "72x72",
-            "type": "image/png"
-        },
-        {
-            "src": "/assets/img/logo.png",
-            "sizes": "114x114",
-            "type": "image/png"
-        },
-        {
-            "src": "/assets/img/logo.png",
-            "sizes": "192x192",
-            "type": "image/png"
-        },
-        {
-            "src": "/assets/img/logo.png",
-            "sizes": "256x256",
-            "type": "image/png"
-        },
-        {
-            "src": "/assets/img/logo.png",
-            "sizes": "512x512",
-            "type": "image/png"
-        }
-    ]
+    "icons": [{
+        "src": "assets/img/logo.png",
+        "sizes": "16x16 32x32 64x64 128x128 256x256 512x512"
+      }, {
+        "src": "assets/img/logo.png",
+        "sizes": "16x16",
+        "type": "image/png"
+      }, {
+        "src": "assets/img/logo.png",
+        "sizes": "32x32",
+        "type": "image/png"
+      }, {
+        "src": "assets/img/logo.png",
+        "sizes": "64x64",
+        "type": "image/png"
+      }, {
+        "src": "assets/img/logo.png",
+        "sizes": "128x128",
+        "type": "image/png"
+      }, {
+        "src": "assets/img/logo.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      }, {
+        "src": "assets/img/logo.png",
+        "sizes": "256x256",
+        "type": "image/png"
+      }, {
+        "src": "assets/img/logo.png",
+        "sizes": "521x512",
+        "type": "image/png"
+      }]
 }
